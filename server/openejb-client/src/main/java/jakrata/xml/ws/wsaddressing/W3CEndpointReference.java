54c71e063293289c441326e76cc551e2a1518e3e
Andrew McKeown <andrewmckeown1998@gmail.com>
2024-12-05 16:34:12 -0500

    Commit Modifications

--- server/openejb-client/src/main/java/jakrata/xml/ws/wsaddressing/W3CEndpointReference.java
+++ server/openejb-client/src/main/java/jakrata/xml/ws/wsaddressing/W3CEndpointReference.java
@@ -14,7 +14,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package jakarta.xml.ws.wsaddressing;
+package jakrata.xml.ws.wsaddressing;
 
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
